49a9414f187fc3d0f6e4cda537435a3cf1795a96
mmarcus <mmarcus@captechventures.com>
2014-12-26 17:10:24 -0500

    Implementing Banner fetch

--- js/app.js
+++ js/app.js
@@ -2,30 +2,56 @@ $(function(){
     console.log("Ready!");
     
     // Categories
-    
     var Category = Backbone.Model.extend();
-    
     var CategoryCollection = Backbone.Collection.extend({
         model: Category,
         url: "http://html5news.herokuapp.com/articles/categories"
     });
-    
     var CategoryListView = Backbone.View.extend({
         tagName: "ul",
         initialize: function() {
             this.model.bind("reset", this.render, this);
         },
         render: function() {
-            _.each(this.model.models, function (category) {
-                $(this.el).append(new CategoryListItemView({model:category}).render().el);
+            _.each(this.model.models, function (model) {
+                $(this.el).append(new CategoryListItemView({model:model}).render().el);
             }, this);
             return this;
         }
     });
-    
     var CategoryListItemView = Backbone.View.extend({
         tagName: "li",
         template: _.template($('#category-template').html()),
+        initialize: function() {
+            this.listenTo(this.model, "change", this.render);
+        },
+        render: function (eventName) {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    // Banners
+    var Banner = Backbone.Model.extend();
+    var BannerCollection = Backbone.Collection.extend({
+        model: Banner,
+        url: "http://html5news.herokuapp.com/banners"
+    });
+    var BannerView = Backbone.View.extend({
+        tagName: "div",
+        initialize: function() {
+            this.model.bind("reset", this.render, this);
+        },
+        render: function() {
+            _.each(this.model.models, function (model) {
+                $(this.el).append(new BannerItemView({model:model}).render().el);
+            }, this);
+            return this;
+        }
+    });
+    var BannerItemView = Backbone.View.extend({
+        tagName: "p",
+        template: _.template($('#banner-template').html()),
         initialize: function() {
             this.listenTo(this.model, "change", this.render);
         },
@@ -39,20 +65,27 @@ $(function(){
     var AppView = Backbone.View.extend({
         el: $("#html5challenge"),
         initialize: function() {
+            var self = this;
+            
             // this.footer = this.$('footer');
             // this.main = this.$('main');
             // this.header = this.$('header');
             this.categoryList = new CategoryCollection();
-            this.categoryListView = new CategoryListView({model:this.categoryList});
-            var self = this;
             this.categoryList.fetch({
-                success: function() {
-                    self.render();
+                success: function(model) {
+                    //self.render();
+                    var categoryListView = new CategoryListView({model:model});
+                    $('#categories-nav').html(categoryListView.render().el);
                 }
             });
-        },
-        render: function() {
-            $('#categories-nav').html(this.categoryListView.render().el);
+
+            this.banners = new BannerCollection();
+            this.banners.fetch({
+                success: function(model) {
+                    var bannerView = new BannerView({model:model});
+                    $('#banner-content').html(bannerView.render().el);
+                }
+            });
         }
     });
     
